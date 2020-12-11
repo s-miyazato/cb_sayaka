dd9b586f941828afe1d04f37f26870478ba720a1
宮里 紗矢香 <saya02storm3104@gmail.com>
2020-12-11 15:18:08 +0900

    クライアント情報ページ追加

--- app.rb
+++ app.rb
@@ -23,7 +23,17 @@ client = PG::connect(
   :user => ENV.fetch("USER", "s.miyazato"), :password => '',
   :dbname => "cb_sayaka")
 
-
+before do
+  if session[:user]
+    session[:user] = client.exec_params("SELECT * FROM initial_registration WHERE id = '#{session[:user]["id"]}'").to_a.first
+    if session[:user]["img"].nil?
+      @img_name = "add-img-personal.jpg"
+    else
+      @img_name = session[:user]["img"]
+    end
+  end
+  
+end
 # ログインページ
 get '/' do
   @title = 'ログイン'
@@ -38,7 +48,7 @@ post '/' do
     return erb :login, :layout => :login_layout
   else
     session[:user] = user
-    binding.irb
+    
     return redirect '/design'
   end
 end
@@ -67,10 +77,10 @@ end
 get '/profile' do
   @title = 'プロフィール'
   current_user = session[:user]
-  @img = current_user["img"]
   @name = current_user["name"]
   @email = current_user["email"]
   @password = current_user["password"]
+  
   return erb :profile
 end
 
@@ -79,23 +89,27 @@ post '/profile' do
   name = params[:name]
   email = params[:email]
   password = params[:password]
-  current_user = session[:user]["name"]
+  current_user = session[:user]
 
   if !params[:img].nil? # データがあれば処理を続行する
     tempfile = params[:img][:tempfile] # ファイルがアップロードされた場所
     save_to = "./public/asset/img/profile/#{params[:img][:filename]}" # ファイルを保存したい場所
     FileUtils.mv(tempfile, save_to)
     @img_name = params[:img][:filename]
+    img = params[:img][:filename]
   end
-  current_user = session[:user]
 
-  client.exec_params("UPDATE initial_registration SET name = $1, email = $2, password = $3, img = $4 WHERE id = '#{current_user["id"]}';", [name, email, password, img])
-
-  return redirect :profile
+  client.exec_params("UPDATE initial_registration SET name = $1, email = $2, password = $3, img = $4 WHERE id = '#{current_user["id"]}'", [name, email, password, img])
+  @img_name = session[:user]["img"]
+  return redirect :design
 end
 
 get '/design' do
   @title = 'デザイン登録'
-
   return erb :design
+end
+
+get '/client-info' do
+  @title = 'クライアント情報'
+  return erb :client_info
 end
